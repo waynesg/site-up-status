78c38f51a91d860bfc4b832c47b645227ac03bc2
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2021-12-18 16:27:28 +0800

    Update toogle.js

--- js/toogle.js
+++ js/toogle.js
@@ -1,20 +1,18 @@
-var anniu =document.getElementById('anniu');
-var kaiguan = document.querySelector('.kaiguan');
-var body = document.getElementById('body');
-var temp = 1;
-anniu.addEventListener('click',function(){
-    if(temp==1){
-        this.className = 'hei';
-        temp=0;
-        kaiguan.style. border= '2px solid  rgb(11, 243, 81)';
-        body.style. backgroundColor= 'rgb(7, 7, 29)';
-        body.style.color = 'white';
-    }else{
-        this.className = 'bai';
-        temp=1;
-        kaiguan.style. border= '2px solid black';
-        body.style. backgroundColor= 'white';
-        body.style.color = 'black';
-     }
+const chk = document.getElementById('chk');
 
-})
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
