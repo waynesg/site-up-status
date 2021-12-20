78267e1a7265b89aa2028c28bb7c0d15a3f311da
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2021-12-20 09:29:51 +0800

    Update toogle.js

--- js/toogle.js
+++ js/toogle.js
@@ -1,18 +1,29 @@
-const chk = document.getElementById('chk');
+// Selecting all the required classes from HTML to change 
+var body = document.body
+var check = document.querySelector('#check')
+var box = document.querySelector('.box')
+var ball = document.querySelector('.ball')
 
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
-});
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
 
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
 
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
+// Adding an eventListener function to change color everytime var check is changed.(checked & unchecked)
+
+check.addEventListener('change',function(){
+
+  //   conditions to apply when checkbox is checked
+
+  if(this.checked == true){
+    box.setAttribute('style','background-color:white;')
+    ball.setAttribute('style','transform:translatex(100%);')
+    body.setAttribute('style','background-color:black; color:white;') 
+  }
+
+  //   conditions to apply when checkbox is unchecked
+
+  if(this.checked == false){
+    box.setAttribute('style','background-color:black; color:white;')
+    ball.setAttribute('style','transform:translatex(0%);')
+    body.setAttribute('style','background-color:white; color:black;')
+  }
+})
